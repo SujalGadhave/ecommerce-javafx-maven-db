04ee5552361607d09a457718c838634e70b04e1e
Sujal Gadhave <sujalgadhave009@gmail.com>
2025-01-10 18:07:29 +0530

    Initial commit

--- src/main/java/database/SqlData.java
+++ src/main/java/database/SqlData.java
@@ -2,6 +2,6 @@ package database;
 
 public class SqlData {
 	public final String url = "jdbc:mysql://127.0.0.1:3306/ecomlogin";
-	public final String username = "root";
-	public final String sqlpass= "Sujal@123";
+	public final String username = "****";
+	public final String sqlpass= "*********";
 }
